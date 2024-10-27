c1a6df998e31a4972215d5efb445f282a3bafb2a
stefan-zugravel <stefanzugravel@gmail.com>
2024-10-27 16:07:22 +0100

    added second ratio

--- get_ratio.C
+++ get_ratio.C
@@ -19,44 +19,69 @@ void CalculateRatios( int irun, TString side = "", TString channel = "")
   int bin_max = h->FindBin(3 + media);
   int BIN_low = h->FindBin(5.5 + media);
   int BIN_max = h->FindBin(9.5 + media);
+  int BIN2_low = h->FindBin(-11.5 + media);
+  int BIN2_max = h->FindBin(-8.5 + media);
   std::cout << "===================== ZN" << TString::Format("%s_%s_%d",side.Data(), channel.Data(),irun) << " ====================="<< std::endl;
-  double integralMain  = h->Integral(bin_low, bin_max);
-  double integralNoise = h->Integral(BIN_low, BIN_max);
-  double ratio = 100*(integralNoise/integralMain);
-  std::cout << "Valor medio  : " << media << std::endl;
+  double integralMain   = h->Integral(bin_low, bin_max);
+  double integralNoise  = h->Integral(BIN_low, BIN_max);
+  double integralNoise2 = h->Integral(BIN2_low, BIN2_max);
+  double ratio  = 100*(integralNoise/integralMain);
+  double ratio2 = 100*(integralNoise2/integralMain);
+  std::cout << "Valor medio   : " << media << std::endl;
   std::cout << "Integrating from " << -3 + media << " and " << 3 + media << std::endl;
-  std::cout << "integralMain : " << integralMain << std::endl;
+  std::cout << "integralMain  : " << integralMain << std::endl;
   std::cout << "Integrating from " << 5.5 + media << " and " << 9.5 + media << std::endl;
-  std::cout << "integralNoise: " << integralNoise << std::endl;
-  std::cout << "ratio        : " << ratio << " %" << std::endl;
+  std::cout << "integralNoise : " << integralNoise << std::endl;
+  std::cout << "Integrating from " << -11.5 + media << " and " << -8.5 + media << std::endl;
+  std::cout << "integralNoise2: " << integralNoise2 << std::endl;  
+  std::cout << "ratio         : " << ratio << " %" << std::endl;
+  std::cout << "ratio2        : " << ratio2 << " %" << std::endl;
 
-  TLine *line0 = new TLine(-3 + media, h->GetMinimum(), -3 + media, h->GetMaximum());
+  TLine *line0 = new TLine(-3 + media, h->GetMinimum(), -3 + media, h->GetMaximum()/2);
   line0->SetLineColor(kRed); // Set line color to red
   line0->SetLineWidth(3);    // Set line width
   line0->Draw("same");
 
-  TLine *line1 = new TLine(3 + media, h->GetMinimum(), 3 + media, h->GetMaximum());
+  TLine *line1 = new TLine(3 + media, h->GetMinimum(), 3 + media, h->GetMaximum()/2);
   line1->SetLineColor(kRed); // Set line color to red
   line1->SetLineWidth(3);    // Set line width
   line1->Draw("same");
 
-  TLine *line2 = new TLine(5.5 + media, h->GetMinimum(), 5.5 + media, h->GetMaximum());
+  TLine *line2 = new TLine(5.5 + media, h->GetMinimum(), 5.5 + media, h->GetMaximum()/2);
   line2->SetLineColor(kBlue); // Set line color to red
   line2->SetLineWidth(3);    // Set line width
   line2->Draw("same");
 
-  TLine *line3 = new TLine(9.5 + media, h->GetMinimum(), 9.5 + media, h->GetMaximum());
+  TLine *line3 = new TLine(9.5 + media, h->GetMinimum(), 9.5 + media, h->GetMaximum()/2);
   line3->SetLineColor(kBlue); // Set line color to red
   line3->SetLineWidth(3);    // Set line width
   line3->Draw("same");
 
+  TLine *line4 = new TLine(-11.5 + media, h->GetMinimum(), -11.5 + media, h->GetMaximum()/2);
+  line4->SetLineColor(kBlack); // Set line color to red
+  line4->SetLineWidth(3);    // Set line width
+  line4->Draw("same");
+
+  TLine *line5 = new TLine(-8.5 + media, h->GetMinimum(), -8.5 + media, h->GetMaximum()/2);
+  line5->SetLineColor(kBlack); // Set line color to red
+  line5->SetLineWidth(3);    // Set line width
+  line5->Draw("same");
+
   std::ostringstream oss;
-  oss << "Ratio: " << ratio << " %   Run number: " << irun;
+  oss << "Ratio R: " << ratio << " %   Run number: " << irun;
 
   TText *text = new TText(-12, h->GetMaximum(), oss.str().c_str());
   text->SetTextColor(kBlack);  // Set text color
   text->SetTextSize(0.04);      // Set the text size
   text->Draw();
+
+  std::ostringstream oss2;
+  oss2 << "Ratio L: " << ratio2 << " % ";
+
+  TText *text2 = new TText(-12, h->GetMaximum()/2, oss2.str().c_str());
+  text2->SetTextColor(kBlack);  // Set text color
+  text2->SetTextSize(0.04);      // Set the text size
+  text2->Draw();
 
 
   c1->Print(TString::Format("results/%d/ratio/Background_study_ZN%s_%s_%d.pdf",irun,side.Data(), channel.Data(),irun));
